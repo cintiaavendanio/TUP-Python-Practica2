fee4e9a92c2fe9cde01fd40f98757e729e454d49
cinti.av <cin899a@gmail.com>
2023-04-24 02:36:30 -0300

    ok

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,18 +1,15 @@
 """Bloque IF, operadores lógicos, función max y operador ternario."""
-
-
-def maximo_basico(a: float, b: float) -> float:
-    if a>b:
-        return a
-    return b
-
-    """Toma dos números y devuelve el mayor.
+"""Toma dos números y devuelve el mayor.
 
     Restricciones:
         - Utilizar IF
         - No utilizar ELSE
         - No utilizar la función max
     """
+def maximo_basico(a: float, b: float) -> float:
+    if a>b:
+        return a
+    return b
 
 # NO MODIFICAR - INICIO
 assert maximo_basico(10, 5) == 10
@@ -21,14 +18,11 @@ assert maximo_basico(9, 18) == 18
 
 
 ###############################################################################
-
-
-def maximo_libreria(a: float, b: float) -> float:
-    return max(a,b)
-    """Re-escribir utilizando el built-in max.
+"""Re-escribir utilizando el built-in max.
     Referencia: https://docs.python.org/3/library/functions.html#max
     """
-
+def maximo_libreria(a: float, b: float) -> float:
+    return max(a,b)
 
 # NO MODIFICAR - INICIO
 assert maximo_libreria(10, 5) == 10
@@ -37,14 +31,11 @@ assert maximo_libreria(9, 18) == 18
 
 
 ###############################################################################
-
-
-def maximo_ternario(a: float, b: float) -> float:
-    return a if a>b else b
-    """Re-escribir utilizando el operador ternario.
+"""Re-escribir utilizando el operador ternario.
     Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions # noqa: E501
     """
-
+def maximo_ternario(a: float, b: float) -> float:
+    return a if a>b else b
 
 # NO MODIFICAR - INICIO
 assert maximo_ternario(10, 5) == 10
